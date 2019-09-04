8262c7f47a517998f81ebcf29e593041a2d49d85
VADIM <kurdayvadim@gmail.com>
2019-09-05 00:46:45 +0400

    Первичный вариант верстки тренировочного макета

---
+++ app/js/common.js
@@ -0,0 +1,24 @@
+$(function() {
+  $("#js-get-started").on("click", function(e){
+    e.preventDefault();
+    var sliderOffset = $("#slider").offset().top;
+
+    $("html, body").animate({
+      scrollTop: sliderOffset
+    }, 500);
+  });
+});
+
+var mainH = $("#js-main").height(),
+    navH = $("#js-nav-container").innerHeight();
+
+$(document).on("scroll", function(){
+  var documentScroll = $(this).scrollTop();
+  if (documentScroll > mainH) {
+    $("#js-nav-container").addClass("fixed");
+    $("#js-main").css("paddingTop", navH);
+  } else {
+    $("#js-nav-container").removeClass("fixed");
+    $("#js-main").removeAttr("style");
+  }
+});
